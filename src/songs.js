1756b74cf3d4603526ac1e3910954dd7bd5d8bfe
Tomas Maldonis <tomasis7@gmail.com>
2025-05-12 21:19:29 +0200

    feat: implement getSongs and deleteSong functions for song management

--- src/songs.js
+++ src/songs.js
@@ -9,3 +9,21 @@ function addSong(title, artist, genre) {
   songs.push(song);
   return song;
 }
+
+function getSongs() {
+  return songs;
+}
+
+function deleteSong(title) {
+  const index = songs.findIndex((song) => song.title === title);
+  if (index !== -1) {
+    return songs.splice(index, 1)[0];
+  }
+  return null;
+}
+
+module.exports = {
+  addSong,
+  getSongs,
+  deleteSong,
+};
